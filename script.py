a7b71b5bb1db161025623e37c8009b70657acfec
manaswii <manaswi.dewan@gmail.com>
2023-04-17 01:06:41 +0530

    retrieve last id from sql table to contnue today

--- script.py
+++ script.py
@@ -16,25 +16,24 @@ class VergeScraper:
         authors = soup.find_all('a',class_='text-gray-31 hover:shadow-underline-inherit dark:text-franklin mr-8')
         dates = soup.find_all('span',class_='text-gray-63 dark:text-gray-94')
         urls = soup.find_all('a',class_='group-hover:shadow-underline-franklin')
-        titles = []
-        authors = []
-        dates = []
-        urls = []
+        final_titles = []
+        final_authors = []
+        final_dates = []
+        final_urls = []
         for title in titles:
             title = title.text
-            titles.append(title)
+            final_titles.append(title)
         for author in authors:
             author = author.text
-            authors.append(author)
+            final_authors.append(author)
         for date in dates:
             date = date.text
-            dates.append(date)
+            final_dates.append(date)
         for url in urls:
             url = self.url + url['href']
-            urls.append(url)
-
-        for i in range(len(titles)):
-            article_dict = {'headline' : titles[i], 'url' : urls[i], 'author' : authors[i+1], 'date_posted' : dates[i+1]}
+            final_urls.append(url)
+        for i in range(len(final_titles)):
+            article_dict = {'headline' : final_titles[i], 'url' : final_urls[i], 'author' : final_authors[i+1], 'date_posted' : final_dates[i+1]}
             article_list.append(article_dict)
         return article_list
     
@@ -61,10 +60,11 @@ class VergeScraper:
         c.execute('''CREATE TABLE IF NOT EXISTS articles
                      (id INTEGER PRIMARY KEY, url TEXT, headline TEXT, author TEXT, date TEXT)''')
         res = c.execute('''SELECT id FROM articles ORDER BY id DESC LIMIT 1;''')
-        lastId = res.fetchone()[0]
-        for i, article in enumerate(article_list):
+        lastId = res.fetchone()
+        lastId = lastId[0] if lastId is not None else 0
+        for article in (article_list):
             lastId += 1
-            c.execute("INSERT OR IGNORE INTO articles (id, url, headline, author, date) VALUES (?, ?, ?, ?, ?)", (i, article['url'], article['headline'], article['author'], article['date_posted']))
+            c.execute("INSERT OR IGNORE INTO articles (id, url, headline, author, date) VALUES (?, ?, ?, ?, ?)", (lastId, article['url'], article['headline'], article['author'], article['date_posted']))
         conn.commit()
         conn.close()
 if __name__ == "__main__":
